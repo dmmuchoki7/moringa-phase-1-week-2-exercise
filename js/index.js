a173c27f8cb70de4c26488482a19ac90fd7af3be
SamTomashi <samtomashikayembi@yahoo.fr>
2023-10-19 17:31:18 +0300

    add and remove to cart

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let items = [];
+let cart = [];
 document.addEventListener("DOMContentLoaded", async function(){
    const response = await getItems()
-   displayItems(response)
+   items = [...response]
+   displayItems(items)
 })
 
  function getItems(){
@@ -14,7 +16,7 @@ function displayItems(items){
     const itemContainer = document.querySelector("#items");
     for(item of items){
         itemContainer.innerHTML += `
-        <div class="p-2 m-1 col-3">
+        <div class="ml-1 my-2 col-4">
             <div class="card" >
                 <img src="${item.image}" height="250px" class="card-img-top image" alt="...">
                 <div class="card-body">
@@ -26,17 +28,61 @@ function displayItems(items){
                  <li>Price: ${item.price}</li>
                 </ul>
                 </span>
-                <form class="w-100">
+                <form onsubmit="addToCart(event, ${item.id})"  class="frm_cart w-75">
                     <label>QTY</label>
-                    <input type="number" min="1" value="1"/>
-                    <button class="mx-auto w-100 btn btn-primary m-2">Add to cart</button>
+                    <input name="qty" type="number" min="1" value="1"/>
+                    <button  class="btn btn-primary btn-sm btn-block m-4 w-100" type="submit" class="mx-auto w-100 btn btn-primary m-2">Add to cart</button>
                 </form>
                 </div>
             </div>
         </div>
         `
     }
+
 }
+
+function addToCart(event, id){
+    event.preventDefault();
+    const frm = new FormData(event.target);
+    const selectedItem = items.find(item => item.id === parseInt(id))
+    const data =  {
+        qty_in_order: frm.get('qty'),
+        total_price: parseFloat(frm.get('qty')) * parseFloat(selectedItem.price),
+        ...selectedItem
+    };
+    cart.push(data)
+    displayItemsToCart(cart)
+}
+
+function displayItemsToCart(items){
+    document.querySelector("#cart_body").innerHTML = ""
+    cart.forEach((item)=> {
+        document.querySelector("#cart_body").innerHTML +=`
+        <tr>
+            <td>${item.id}</td>
+            <td>${item.name}</td>
+            <td>${item.qty_in_order}</td>
+            <td>Ksh${item.total_price}</td>
+            <td>
+                <button class="btn btn-sm btn-danger" onclick="removeItemFromCart(event, ${item.id})">X</button>
+            </td>
+        </tr>
+        `
+    })
+}
+
+
+function removeItemFromCart(event, id){
+   event.target.closest('tr').remove();
+    const newCart = cart.filter((item => item.id !== parseInt(id)))
+    cart = newCart
+
+}
+
+
+
+
+
 
 
 // document.querySelector("#frm").addEventListener("submit", function(event){
