b3407cdf582110d8ee670134f2c3b9e8e23bb093
SamTomashi <samtomashikayembi@yahoo.fr>
2023-10-19 12:07:51 +0300

    fetch items from DB

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
-/**
- * GET: Retrirve data form the server
- * POST: POST data to the server
- * PUT/PACTH: to update data on the server
- * DELETE: DELETE data from the server
- * 
- */
+let items = [];
+document.addEventListener("DOMContentLoaded", function(){
+    getItems()
+})
+
+function getItems(){
+    fetch("http://localhost:3000/items")
+    .then(response => response.json())
+    .then(data => {
+        items = [...data]
+        displayItems(data)
+    })
+}
+
+
+
+function displayItems(items){
+    const itemContainer = document.querySelector("#items");
+    for(item of items){
+        itemContainer.innerHTML += `
+        <div class="p-2 m-1 col-3">
+            <div class="card" >
+                <img src="${item.image}" height="250px" class="card-img-top image" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <span class="card-text">
+                <ul>
+                 <li>Color: ${item.color}</li>
+                 <li>QTY Available: ${item.qty}</li>
+                 <li>Price: ${item.price}</li>
+                </ul>
+                </span>
+                <form>
+                    <label>QTY</label>
+                    <input type="number" min="1" value="1"/>
+                    <button class="btn btn-primary btn-block m-2 btn-lg">Add to cart</button>
+                </form>
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
